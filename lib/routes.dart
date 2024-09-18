95cc5db74e406d6da6683cb5c72094d2ce841a01
jeetubhaiya5529 <jeetendrakumar5529@gmail.com>
2024-09-18 18:56:01 +0530

    design welcome screen

---
+++ lib/routes.dart
@@ -0,0 +1,14 @@
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => AuthScreen());
+    default: return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => Scaffold(body: Center(child: Text('Error 404, Page not found'),),));
+  }
+}
